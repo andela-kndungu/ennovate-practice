732aae58d0786bca04867360ec2eac071ef45e35
Kinuthia Ndung'u <kinuthia.ndungu@andela.com>
2016-06-23 17:32:52 +0300

    Turn tabs to spaces

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
 const express = require('express'),
-	app = express(),
-	bodyParser = require('body-parser'),
-	router = require('./server/router'),
-	port = process.env.NODE_PORT || 8000,
-	ip = process.env.NODE_IP || 'localhost';
+  app = express(),
+  bodyParser = require('body-parser'),
+  router = require('./server/router'),
+  port = process.env.NODE_PORT || 8000,
+  ip = process.env.NODE_IP || 'localhost';
 
 // Set up bodyParser to get passed parameters and post bodies as objects
 app.use(bodyParser.urlencoded({
-	extended: true
+  extended: true
 }));
 
 app.use(bodyParser.json());
@@ -17,6 +17,6 @@ router(app);
 
 // Start receiving requests
 app.listen(port, ip, function() {
-	console.log('Listening on port ' + port);
+  console.log('Listening on port ' + port);
 });
 
